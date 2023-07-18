4d4bb8e2187286f5b1115c094f633603b167aace
hasuhwan <hapung215@gmail.com>
2023-07-18 22:23:29 +0900

    중복 요소 묶기

--- src/SquareDrawing.js
+++ src/SquareDrawing.js
@@ -1,9 +1,7 @@
 import SquareLayout from "./SquareLayout";
 export default function SquareDrawing(props) {
   return (
-    <div
-      className={`square-drawing w-full h-full row-span-2 col-span-2 outline outline-4  ountline-blackColor ${props.step.value}`}
-    >
+    <div className={`square-drawing ${props.step.value}`}>
       {props.step.next.length !== 0 ? <SquareLayout step={props.step} /> : null}
     </div>
   );
--- src/SquareLayout.js
+++ src/SquareLayout.js
@@ -1,7 +1,7 @@
 import SquareDrawing from "./SquareDrawing";
 export default function SquareLayout(props) {
   return (
-    <div className="square-layout w-full h-full grid grid-cols-4 grid-rows-4 gap-[4px]">
+    <div className="square-layout">
       {props.step.next.map((el, idx) => {
         return <SquareDrawing key={idx} step={el} />;
       })}
